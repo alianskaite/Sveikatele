6c90f258706ff25ac0871e5fe63957c8a7cd6fb0
Gabija Alianskaitė <alianskaite@gmail.com>
2024-04-14 21:18:25 +0300

    projektas

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tp_sveikatele.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
