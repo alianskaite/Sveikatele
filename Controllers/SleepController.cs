bdce5c5f155f25a990db070501aa76a3bb527b96
Gabija Alianskaitė <alianskaite@gmail.com>
2024-04-29 20:28:57 +0300

    sleep tracker functionality

---
+++ Controllers/SleepController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using tp_sveikatele.Models;
+using tp_sveikatele.Repositories;
+
+namespace tp_sveikatele.Controllers
+{
+    public class SleepController : Controller
+    {
+        // In-memory collections to store posts and comments
+        private static List<SleepL> sleepData = new List<SleepL>();
+
+        public IActionResult Index()
+        {
+            // Retrieve posts with their associated comments
+            var sleep = SleepRepo.List();
+            return View(sleep);
+        }
+    }
+}
